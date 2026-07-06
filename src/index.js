05f36d568a3e41e5ef6ca0c347dc7148e814d3e4
Om <ojjadhav.007@gmail.com>
2026-07-06 21:51:02 +0530

    utils code discussion

--- src/index.js
+++ src/index.js
@@ -1,23 +1,24 @@
-// require('dotenv').config({path: './env'})
-4
-import dotenv from "dotenv"
+// require('dotenv').config({path: './env'});
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
 dotenv.config({
-    path: './.env'
-})
+  path: "./.env",
+});
 
 connectDB()
-
-
-
-
-
-
-
-
-
-
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("ERROR", error);
+      throw error;
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Serer is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGODB db connection failed  !! ", err);
+  });
 
 /*
 
