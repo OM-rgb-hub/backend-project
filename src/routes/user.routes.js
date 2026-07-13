9049774674fb4a8f43dc03f809e634a1a9e72d12
Om <ojjadhav.007@gmail.com>
2026-07-13 23:32:20 +0530

    error handling

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,28 +1,31 @@
-import {Router} from "express"
-import { loginUser, logoutUser,registerUser ,refreshToken} from "../controllers/user.controller.js"
-import {upload} from "../middlewares/multer.middlewares.js"
-import { verifyJWT } from "../middlewares/auth.middlewares.js"
+import { Router } from "express";
+import {
+  loginUser,
+  logoutUser,
+  registerUser,
+  refreshToken,
+} from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middlewares.js";
+import { verifyJWT } from "../middlewares/auth.middlewares.js";
 
-const router = Router()
+const router = Router();
 
 router.route("/register").post(
-    upload.fields([
-        {
-            name:"avatar",
-            maxCount:1
-        },
-        {
-            name: "coverImage",
-            maxCount:1
-        }
-    ]),
-    registerUser
-)
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  registerUser
+);
 
+router.route("/login").post(loginUser);
 
-router.route("/login").post(loginUser)
-
-router.route("/logout").post(verifyJWT,logoutUser)
-router.route("/refresh-token").get(verifyJWT,refreshToken
-)
-export default router
+router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/refresh-token").get(verifyJWT, refreshToken);
+export default router;
